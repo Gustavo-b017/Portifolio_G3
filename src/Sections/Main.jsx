c7d978325fa9b26182f054dcbb48c1f886f2df08
Gustavo <gustavo.estudo017@gmail.com>
2025-11-04 22:07:11 -0300

    main mobile

--- src/Sections/Main.jsx
+++ src/Sections/Main.jsx
@@ -1,6 +1,33 @@
+import React from "react";
+import logo from "/public/imgs/logo-svg.svg";
+import "/public/style/main.scss";
+
 function Main() {
-  return ( 
-    <h1>main</h1>
+  return (
+    <section id="inicio" className="cubo">
+      {/* LADO ESQUERDO: TÍTULO + TEXTO */}
+      <div className="cubo_text">
+        <h1 className="cubo_title" aria-label="Pensar ao cubo é ir além">
+
+          <p>
+            <span className="laranja">PENSAR</span> AO
+          </p>
+
+          <p>CUBO É IR</p>
+
+          <p className="azul">ALÉM</p>
+        </h1>
+
+        <p className="cubo_lead">
+          Conheça nossos projetos, habilidades e o que nos inspira.
+          Sinta a conexão entre nossa experiência e nossa paixão por criar.
+        </p>
+      </div>
+
+      <div className="cubo_art">
+        <img src={logo} alt="Cubo isométrico" />
+      </div>
+    </section>
   );
 }
 
